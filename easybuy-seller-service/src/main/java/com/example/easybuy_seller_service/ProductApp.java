8210d7a36f5decc5ac846e6fec5d8c36281c4c51
pavan8123 <bantakuntapavan@gmail.com>
2019-03-21 12:23:32 +0530

    Seller commiting

---
+++ easybuy-seller-service/src/main/java/com/example/easybuy_seller_service/ProductApp.java
@@ -0,0 +1,14 @@
+package com.example.easybuy_seller_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ProductApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApp.class, args);
+	}
+}
